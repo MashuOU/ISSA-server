01aa4b2702cc47a5f418e4ca1ee7c7ebadf10d46
dmsardim <dimasardiyanto19@gmail.com>
2023-03-03 11:33:08 -0600

    fix: student by specific class in CMS

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const TeacherController = require('../controllers/teacherController')
-const router = express.Router()
+const express = require('express');
+const TeacherController = require('../controllers/teacherController');
+const router = express.Router();
 
-router.post('/teacher/login', TeacherController.login)
+router.post('/teacher/login', TeacherController.login);
 
-module.exports = router
+module.exports = router;
