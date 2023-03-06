1035be7d44e5beb94f8b7670b7bd6c92b967c80a
HusinHanbasy <husinhanbasy88@gmail.com>
2023-03-06 22:33:33 +0700

    feat: history

--- routes/schedule.js
+++ routes/schedule.js
@@ -2,8 +2,8 @@ const express = require('express');
 const schedule = require('../controllers/scheduleController');
 const router = express.Router();
 
-router.post('/', schedule.addSchedule);
-router.get('/', schedule.schedules);
+// router.post('/', schedule.addSchedule);
+// router.get('/', schedule.schedules);
 
 
 module.exports = router;
--- routes/teacher.js
+++ routes/teacher.js
@@ -4,14 +4,12 @@ const TeacherController = require('../controllers/teacherController');
 const { teacherAuth } = require('../middlewares/authentication');
 
 router.post('/login', TeacherController.login);
-
 router.use(teacherAuth)
 
 router.post('/register', TeacherController.register);
 router.put('/:id', TeacherController.editTeacher)
 
 router.delete('/:id', TeacherController.deleteTeacher)
-
 
 
 module.exports = router;
