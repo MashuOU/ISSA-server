99994937d2ef09fba211c53c19ff26e14fb00599
HusinHanbasy <husinhanbasy88@gmail.com>
2023-03-06 19:45:20 +0700

    feat feature cud class and teacher

--- routes/class.js
+++ routes/class.js
@@ -5,4 +5,9 @@ const classController = require('../controllers/classController');
 router.get('/', classController.fetchAllClass);
 router.get('/:classId', classController.fetchClassById);
 
+router.post('/', classController.addClass)
+router.put('/:id', classController.editClass)
+
+router.delete('/:id', classController.deleteClass)
+
 module.exports = router;
